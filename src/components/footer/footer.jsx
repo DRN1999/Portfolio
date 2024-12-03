872cbb5ffddbcacca95411cc5dc0bc76f0d4e15f
Rahul Dethe <62913308+DRN1999@users.noreply.github.com>
2024-12-04 05:14:59 +0530

    Update footer.jsx
    As per poonam

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ const Footer = (props) => {
   return (
     <footer className="footer">
         <div className="footer__container container">
-            <h1 className="footer__title">Rahul</h1>
+            <h1 className="footer__title">Poonam</h1>
 
             <ul className="footer__list">
                 <li>
@@ -21,13 +21,13 @@ const Footer = (props) => {
             </ul>
 
             <div className="footer__social">
-                <a href="https://discord.com/channels/@drahul.7057" className="footer__social-icon" target="_blank">
-                    <i class="bx bxl-discord"></i>
+                <a href="https://api.whatsapp.com/send?phone=9920733089&text=Hello%2C%20I'm%20Poonam.%20Could%20you%20please%20tell%20me%20something%20about%20your%20project!%3F" className="footer__social-icon" target="_blank">
+                    <i class="bx bxl-whatsapp"></i>
                 </a>
-                <a href="https://github.com/DRN1999" className="footer__social-icon" target="_blank">
-                    <i class="bx bxl-github"></i>
-                </a>
-                <a href="http://linkedin.com/in/rahul-n-dethe1999" className="footer__social-icon" target="_blank">
+{/*                 <a href="https://github.com/DRN1999" className="footer__social-icon" target="_blank">
+                    <i class="bx bxl-git"></i>
+                </a> */}
+                <a href="https://www.linkedin.com/in/poonam-mhaske-508499306/" className="footer__social-icon" target="_blank">
                     <i class="bx bxl-linkedin"></i>
                 </a>
             </div>
