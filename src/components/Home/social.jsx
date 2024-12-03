4223d827888d9d7031592f0caed5143e1cf18642
Rahul Dethe <62913308+DRN1999@users.noreply.github.com>
2024-12-04 04:08:26 +0530

    Update social.jsx
    update according to poonam

--- src/components/Home/social.jsx
+++ src/components/Home/social.jsx
@@ -3,13 +3,13 @@
 const Social = (props) => {
   return (
     <div className="home__social">
-      <a href="https://discord.com/channels/@drahul.7057" className="home__social-icon" target="_blank">
-      <i class="uil uil-discord"></i>
+      <a href="https://api.whatsapp.com/send?phone=9920733089&text=Hello%2C%20I'm%20Poonam.%20Could%20you%20please%20tell%20me%20something%20about%20your%20project!%3F" className="home__social-icon" target="_blank">
+      <i class="uil uil-whatsapp"></i>
       </a>
-      <a href="https://github.com/DRN1999" className="home__social-icon" target="_blank">
+      <a href="https://github.com/" className="home__social-icon" target="_blank">
       <i class="uil uil-github"></i>
       </a>
-      <a href="http://linkedin.com/in/rahul-n-dethe1999" className="home__social-icon" target="_blank">
+      <a href="https://www.linkedin.com/in/poonam-mhaske-508499306/" className="home__social-icon" target="_blank">
       <i class="uil uil-linkedin"></i>
       </a>
     </div>
