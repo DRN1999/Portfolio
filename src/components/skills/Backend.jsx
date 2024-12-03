1b3d3c7d4975f2c828deeda842a7636e7f9d9ca4
Rahul Dethe <62913308+DRN1999@users.noreply.github.com>
2024-12-04 04:42:52 +0530

    Update Backend.jsx
    As per poonam

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,14 +3,14 @@
 const Backend = (props) => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Backend Developer</h3>
+      <h3 className="skills__title">Soft Skills</h3>
 
       <div className="skills__box">
         <div className="skills__group">
             <div className="skills__data">
                 <i class='bx bx-badge-check'></i>
                 <div>
-                    <h3 className="skills__name">Node Js</h3>
+                    <h3 className="skills__name">Creativity</h3>
                     <span className="skills__level">Basic</span>
                 </div>
             </div>
@@ -20,7 +20,7 @@ const Backend = (props) => {
             <div className="skills__data">
                 <i class='bx bx-badge-check'></i>
                 <div>
-                    <h3 className="skills__name">Express Js</h3>
+                    <h3 className="skills__name">Time Management</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
             </div>
@@ -30,7 +30,7 @@ const Backend = (props) => {
             <div className="skills__data">
                 <i class='bx bx-badge-check'></i>
                 <div>
-                    <h3 className="skills__name">MongoDB</h3>
+                    <h3 className="skills__name">Teamwork</h3>
                     <span className="skills__level">Basic</span>
                 </div>
             </div>
@@ -40,7 +40,7 @@ const Backend = (props) => {
             <div className="skills__data">
                 <i class='bx bx-badge-check'></i>
                 <div>
-                    <h3 className="skills__name">Python</h3>
+                    <h3 className="skills__name">Adaptability</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
             </div>
