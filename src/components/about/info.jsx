ffda643b5b5e8e15c4bed459d67cf8b183c57bde
Rahul Dethe <62913308+DRN1999@users.noreply.github.com>
2024-12-04 04:33:43 +0530

    Update info.jsx
    As per poonam

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -5,14 +5,14 @@ const Info = (props) => {
     <div className="about__info grid">
       <div className="about__box">
       <i class='bx bx-award about__icon'></i>
-        <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">2 Years Working</span>
+        <h3 className="about__title">Fresher</h3>
+        <span className="about__subtitle">1 Year Working</span>
       </div>
 
       <div className="about__box">
       <i class='bx bx-briefcase-alt about__icon'></i>
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">3 Projects</span>
+        <span className="about__subtitle">4 Projects</span>
       </div>
 
       <div className="about__box">
