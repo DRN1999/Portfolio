c7114b42a89cb59526d0a959f631f5127f389ff0
Rahul Dethe <62913308+DRN1999@users.noreply.github.com>
2024-12-04 12:04:43 +0530

    Update Backend.jsx
    As per poonam

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,7 +3,7 @@
 const Backend = (props) => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Soft Skills</h3>
+      <h3 className="skills__title">Expertise Skills</h3>
 
       <div className="skills__box">
         <div className="skills__group">
